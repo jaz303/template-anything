dc429c8d702d885fc2dab714c827b5a48d15953b
Jason Frame <jason@onehackoranother.com>
2014-09-02 23:31:24 +0100

    carving away at this, we can now run a simple plan

--- cli.js
+++ cli.js
@@ -17,76 +17,78 @@ var path = require('path');
 var parser = require('./lib/parser');
 
 var Context = require('./lib/Context');
+var Environment = require('./lib/Environment');
+var Executor = require('./lib/Executor');
 
-templatePath(template, '.', function(err, path) {
+templatePath(template, '.', function(err, tp) {
 
-	tmp.dir({unsafeCleanup: true}, function(err, tmpDir) {
-		if (err) return;
-		doClone(tmpDir + '/clone');
-	});
-
-	function doClone(templateClonePath) {
-		clone(path.url, templateClonePath, {
-			shallow: true
-		}, function(err) {
-			if (err) return;
-			removeDotGit(templateClonePath);
-		});
+	if (tp.type === 'local') {
+		doTemplate(tp.path, target);
 	}
 
-	function removeDotGit(templateClonePath) {
 
-		rimraf(templateClonePath + '/.git', function(err) {
-			if (err) return;
-			doTemplate(templateClonePath, target);
-		});
+	// tmp.dir({unsafeCleanup: true}, function(err, tmpDir) {
+	// 	if (err) return;
+	// 	doClone(tmpDir + '/clone');
+	// });
 
-	}
+	// function doClone(templateClonePath) {
+	// 	clone(path.url, templateClonePath, {
+	// 		shallow: true
+	// 	}, function(err) {
+	// 		if (err) return;
+	// 		removeDotGit(templateClonePath);
+	// 	});
+	// }
+
+	// function removeDotGit(templateClonePath) {
+
+	// 	rimraf(templateClonePath + '/.git', function(err) {
+	// 		if (err) return;
+	// 		doTemplate(templateClonePath, target);
+	// 	});
+
+	// }
 
 });
 
 function doTemplate(templatePath, targetPath) {
 
-	var N = require('./lib/nodes');
-	var Plan = require('./lib/Plan');
-	var Environment = require('./lib/Environment');
-	var Executor = require('./lib/Executor');
+	var planSource = 'tree';
+	try {
+		planSource = fs.readFileSync(path.join(templatePath, 'plan.tpl'), 'utf8');
+	} catch (e) {
+		// do nothing; use default source code
+	}
+
+	var plan;
+	try {
+		plan = parser.parse(planSource, {startRule: 'Script'});
+	} catch (e) {
+		console.error("error parsing plan!");
+		return;
+	}
+
+	try {
+		mkdirp.sync(targetPath);
+	} catch (e) {
+		console.error("couldn't create target directory: " + targetPath);
+		return;
+	}
 
 	var env = new Environment();
 	require('./lib/directives')(env);
 
-	var plan = null;
+	var context = new Context(env, templatePath, targetPath);
 
-	_readPlan();
-
-	function _readPlan() {
-		fs.readFile(path.join(templatePath, 'plan.tpl'), 'utf8', function(err, src) {
-			_parsePlan(err ? 'tree' : src);
-		});
-	}
-
-	function _parsePlan(src) {
-		try {
-			plan = parser.parse(src, {startRule: 'Script'});
-			_createTargetDir();
-		} catch (e) {
-			console.error("error parsing plan!");
+	var executor = new Executor();
+	executor.run(plan, context, function(err) {
+		if (err) {
+			console.error(err);
+			console.error(err.stack);
+		} else {
+			console.log('all done');
 		}
-	}
-
-	function _createTargetDir() {
-		mkdirp(targetPath, function(err) {
-			if (err) return;
-			_runPlan();
-		});
-	}
-
-	function _runPlan() {
-		var exec = new Executor();
-		exec.run(
-			plan,
-			new Context(env, templatePath, targetPath)
-		);
-	}
+	});
 
 }
